fba18768c8159f06dc9d4dc4e0540991f34f1104
unknown <20130262@st.hcmuaf.edu.vn>
2024-01-03 23:07:44 +0700

    design order site

---
+++ src/navigation/OrderScreen/MyOrdersScreen.js
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { StyleSheet, TabView, TouchableOpacity, View, useWindowDimensions, Image } from 'react-native';
+import { NavigationContainer} from '@react-navigation/native';
+import { useState } from 'react';
+import { Text } from 'react-native';
+import DropDownPicker from 'react-native-dropdown-picker';
+
+import TopTabMenu from './TopTabMenu';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+export default function MyOrdersScreen({ navigation }) {
+ 
+
+    return (
+    
+            <TopTabMenu />
+       
+    );
+}
---
+++ src/navigation/OrderScreen/TopTabMenu.js
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import { StyleSheet, TabView, TouchableOpacity, View, useWindowDimensions, Image } from 'react-native';
+import { NavigationContainer} from '@react-navigation/native';
+import { useState } from 'react';
+import { Text } from 'react-native';
+import DropDownPicker from 'react-native-dropdown-picker';
+import styles from './OrderScreen.style';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import Incoming from './Incoming';
+import History from './History';
+import Rate from './Rate';
+import Checkout from './Checkout';
+
+const Tab = createMaterialTopTabNavigator();
+
+
+
+const TopTabMenu = () => {
+    return (
+        <Tab.Navigator
+        screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+                let iconName;
+                let rn = route.name;
+
+           
+            },
+            tabBarActiveTintColor: 'orangered',
+            tabBarInactiveTintColor: 'gray',
+            
+            
+        })}
+        >
+            <Tab.Screen style={styles.navigator} component={Incoming} name='Đang đến' />
+            <Tab.Screen style={styles.navigator} component={History} name='Lịch sử' />
+            <Tab.Screen style={styles.navigator} component={Rate} name='Đánh giá' />
+            <Tab.Screen style={styles.navigator} component={Checkout} name='Thanh toán' />
+        </Tab.Navigator>
+    )
+}
+
+
+  
+export default TopTabMenu;
