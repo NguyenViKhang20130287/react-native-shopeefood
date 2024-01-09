5a76fae9305e65213f5f7e179fce4edef1737bbb
HoangTrungHieu-20130260 <20130260@st.hcmuaf.edu.vn>
2024-01-09 19:44:02 +0700

    chức năng đăng nhập, đăng ký

--- src/navigation/screens/user/RegisterScreen.js
+++ src/navigation/screens/user/RegisterScreen.js
@@ -1,15 +1,66 @@
 import * as React from 'react';
 import { useState } from 'react';
-import { TouchableOpacity, View, Text, TextInput, Image, ScrollView, Button, KeyboardAvoidingView } from 'react-native';
+import { TouchableOpacity, View, Text, TextInput, Modal } from 'react-native';
 import { FontAwesome } from '@expo/vector-icons';
 import { Ionicons } from '@expo/vector-icons';
 import userStyle from './UserStyle'
+import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 
 export default RegisterScreen = ({navigation}) => {
     const [secureTextEntry, setSecureTextEntry] = useState(true);
+    const [modalVisible, setModalVisible] = useState(false);
+    const [textModal, setTextModal] = useState('');
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [rePassword, setRePassword] = useState('');
+
     const changeHidePassword = () => {
         setSecureTextEntry(!secureTextEntry);
-      };
+    };
+    const showModal = (text) => {
+        setTextModal(text)
+        setModalVisible(true)
+        setTimeout(() => {
+            setModalVisible(false)
+            setTextModal('')
+        }, 1500)
+    }
+    const registerAPI = () => {
+        if (email === '' || password === '' || rePassword === '') {
+            showModal('Vui lòng nhập đầy đủ thông tin!')
+        } else if (password !== rePassword) {
+            showModal('Mật khẩu không trùng khớp!')
+        } else if (password.length < 6) {
+            showModal('Mật khẩu phải trên 6 kí tự!')
+        } else {
+            const apiUrl = 'http://localhost:8080/api/user/register'
+            const requestData = {
+                email : email,
+                password : password,
+            }
+            axios.post(apiUrl, requestData, {
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            })
+            .then(async response => {
+                if (response.data.statusCodeValue === 200) {
+                    const registerData = {email, password}
+                    await AsyncStorage.setItem("register", JSON.stringify(registerData))
+                    navigation.navigate('ConfirmRegister')
+                } else if (response.data.statusCodeValue === 400){
+                    showModal('Email đã tồn tại!')
+                }
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            })
+        }
+       
+    }
     return (
         <View style={userStyle.container}>
             <View style={userStyle.main}>
@@ -18,26 +69,27 @@ export default RegisterScreen = ({navigation}) => {
                 </View>
                 <View style={userStyle.boxInput}>
                     <Ionicons name='mail-outline' style={userStyle.icon}/>
-                    <TextInput style={userStyle.input} placeholder='Email'/>
+                    <TextInput style={userStyle.input} placeholder='Email' onChangeText={text => setEmail(text)}/>
                 </View>
-                <View style={userStyle.boxInput}>
+                {/* <View style={userStyle.boxInput}>
                     <Ionicons name='phone-portrait-outline' style={userStyle.icon}/>
                     <TextInput style={userStyle.input} placeholder='Số điện thoại'/>
-                </View>
+                </View> */}
                 <View style={userStyle.boxInput}>
                     <Ionicons name='lock-closed-outline' style={userStyle.icon}/>
-                    <TextInput style={userStyle.input} placeholder='Mật khẩu' secureTextEntry={secureTextEntry}/>
+                    <TextInput style={userStyle.input} placeholder='Mật khẩu' secureTextEntry={secureTextEntry}
+                        onChangeText={text => setPassword(text)}/>
                     <Ionicons 
                         name={secureTextEntry ? 'eye-off-outline': 'eye-outline'} 
                         style={userStyle.icon}
                         onPress={changeHidePassword}/>
-                    {/* <Text style={userStyle.forgotPassword}>Quên?</Text> */}
                 </View>
                 <View style={userStyle.boxInput}>
                     <Ionicons name='lock-closed-outline' style={userStyle.icon}/>
-                    <TextInput style={userStyle.input} placeholder='Nhập lại mật khẩu' secureTextEntry={secureTextEntry}/>
+                    <TextInput style={userStyle.input} placeholder='Nhập lại mật khẩu' secureTextEntry={secureTextEntry}
+                        onChangeText={text => setRePassword(text)}/>
                 </View>
-                <TouchableOpacity style={userStyle.button} onPress={() => navigation.navigate('ConfirmRegister')}>
+                <TouchableOpacity style={userStyle.button} onPress={registerAPI}>
                         <Text style={userStyle.buttonText}>Đăng ký</Text>
                 </TouchableOpacity>
             </View>
@@ -46,6 +98,22 @@ export default RegisterScreen = ({navigation}) => {
                     <Text style={{color: 'blue'}} onPress={() => navigation.navigate('Login')}> Đăng nhập ngay</Text>
                 </Text>
             </View>
+            <Modal
+                animationType="fade"
+                transparent={true}
+                visible={modalVisible}
+                // onRequestClose={() => {
+                //     Alert.alert('Modal has been closed.');
+                //     setModalVisible(!modalVisible);
+                // }}
+                >
+                <View style={userStyle.centeredView}> 
+                    <View style={userStyle.modalView}>
+                        <Ionicons name='warning-outline' size={50} color={'white'}/>
+                        <Text style={userStyle.modalText}>{textModal}</Text>
+                    </View>
+                </View>
+            </Modal>
         </View>
     );
 }
